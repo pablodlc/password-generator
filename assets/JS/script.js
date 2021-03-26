6ffc33b0e60895acb41c66c57e7a4c2b663fbe27
pablo de la cruz <pablodlc@gmail.com>
2021-03-26 00:04:50 -0700

    cleaned up script.js A LOT

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,29 +1,10 @@
-// Button is clicked which activates a prompt window
-
-//  App must grab the chosen characters, include them in a password, and make sure the length is what the user specified
-// App prints that info to the page so the user can copy/pasta
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+    // var password = generatePassword();
+    // var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
-}
-
-// Add event listener to generate button
-
-// Given starting code `generateBtn.addEventListener("click", writePassword);`
-document.getElementById("generate").addEventListener("click", writePassword);
-
-function writePassword() {
-}
-
-function btnGen() {
-// User chooses the length of the password
     let userLength = prompt("How many characters are to be included in the password?\n8 minimum characters, 128 maximum.", "8");
 
     if (isNaN(userLength) || userLength < 8 || userLength > 128) {
@@ -62,4 +43,9 @@ function btnGen() {
             btnGen();
         }
     }
+    // passwordText.value = password;
 }
+
+document.getElementById("generate").addEventListener("click", writePassword);
+
+// Add event listener to generate button
